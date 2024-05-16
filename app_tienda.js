c34985f06eb5c42280af01170490a4ce05ac923c
julieta <j.gazzin@gmail.com>
2024-05-16 17:28:13 -0300

    carrito + js

--- app_tienda.js
+++ app_tienda.js
@@ -1,5 +1,99 @@
+// contenedor carrito
+const btnCarrito = document.querySelector('nav .carrito i')
+const contenedorCarrito = document.querySelector('.contenedor-carrito')
+
+btnCarrito.addEventListener('click', () => {
+    contenedorCarrito.classList.toggle('hidden')
+})
+
+// carrito
+const rowProducto = document.querySelector('.contenedor-carrito .row-producto')
+const infoProducto = document.querySelector('.contenedor-carrito .info-producto')
+
+const listadoProductos = document.querySelector('.tienda .productos')
+// arreglo de productos seleccionados
+let productosSeleccionados = [];
+
+// total a pagar
+const totalPagar = document.querySelector('.contenedor-carrito .precio-total')
+
+// evento carrito
+listadoProductos.addEventListener('click', e => {
+    if(e.target.classList.contains('comprar')) {
+        let producto = e.target.parentElement.nextElementSibling;
+
+        const datosProducto = {
+            cant: 1,
+            nombre: producto.querySelector('h3').textContent,
+            precio: producto.querySelector('.tags .precio span').textContent
+        }
+        // verificacion producto existentes
+        const existe = productosSeleccionados.some(producto => producto.nombre === datosProducto.nombre)
+        if (existe) {
+            const productos = productosSeleccionados.map(producto =>{
+                if(producto.nombre === datosProducto.nombre) {
+                    producto.cant++;
+                    return producto
+                } else {
+                    return producto
+                }
+            })
+            productosSeleccionados = [...productos]
+        } else {
+            productosSeleccionados = [... productosSeleccionados, datosProducto]
+        }
+
+        console.log(productosSeleccionados);
+        // imprimir en html
+        imprimirHtml()
+    }
+})
+
+// FUNCIONES CARRITO
+function imprimirHtml() {
+    
+    // limpiar html
+    infoProducto.innerHTML = '';
+
+    // total
+    let total = 0;
+    
+    // listados productos seleccionados
+    productosSeleccionados.forEach( producto => {
+        const elementoProducto = document.createElement('div');
+        elementoProducto.classList.add('row-producto');
+        elementoProducto.innerHTML = `
+            <span class="cantidad">${producto.cant}</span>
+            <span class="nombre">${producto.nombre}</span>
+            <span class="precio">$${producto.precio}</span>
+            <span class="btn-eliminar">x</span>
+        `;
+
+        infoProducto.append(elementoProducto);
+
+        // total a pagar
+        total += parseInt(producto.precio) * producto.cant;
+    })
+    totalPagar.innerHTML = `$${total}`
+}
+
+// eliminar producto
+infoProducto.addEventListener('click', (e) => {
+    if(e.target.classList.contains('btn-eliminar')) {
+        const producto = e.target.parentElement;
+        const nombre = producto.children[1].textContent;
+
+        console.log(nombre);
+        productosSeleccionados = productosSeleccionados.filter(producto => producto.nombre !== nombre)
+        console.log(productosSeleccionados);
+        imprimirHtml()
+    }
+})
+
+
+
+// -------    formulario filtros  ---------
 const mostrarTexto= document.querySelectorAll('.productos .plus');
-const zoom = document.querySelectorAll('.img img');
 const btnMenos= document.querySelector('.rango .btn-menos');
 const btnMas= document.querySelector('.rango .btn-mas');
 
@@ -7,10 +101,6 @@ mostrarTexto.forEach(plus => {
     plus.addEventListener('click', mostrar);
 });
 
-
-zoom.forEach( img => {
-    img.addEventListener('click', hacerZoom);
-});
 
 btnMas.addEventListener('click', () => {
     rangoPrecio('mas');
@@ -32,14 +122,6 @@ function mostrar(e) {
     }
     
     detalle.classList.toggle('alto-ofw');
-}
-
-function hacerZoom(e) {
-    console.log(e.target);
-    const img = e.target;
-
-    img.classList.toggle('img-100');
-
 }
 
 function rangoPrecio(params) {
