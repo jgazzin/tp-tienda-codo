3b5480a3eeb440cc260775018a6606814abf3658
julieta <j.gazzin@gmail.com>
2024-05-24 23:51:05 -0300

    ok final

--- blog.js
+++
@@ -1,8 +0,0 @@
-function mostrarRespuesta(id) {
-    var respuesta = document.getElementById(id);
-    if (respuesta.style.display === "none") {
-        respuesta.style.display = "block";
-    } else {
-        respuesta.style.display = "none";
-    }
-}
