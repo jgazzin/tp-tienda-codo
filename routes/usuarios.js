c33a714152e9bf54b7baa1093001bbe98f91de4d
Sabrijdlo <sabrinajaimedelao@hotmail.com>
2024-06-29 19:35:32 -0300

    Creada carpeta publi y organizada. Creada carpeta routes

---
+++ routes/usuarios.js
@@ -0,0 +1,15 @@
+const { json } = require('body-parser');
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) =>
+{
+    res.json(
+        {
+            message : "Hola mundo",
+        }
+    )
+});
+
+module.exports = router;
