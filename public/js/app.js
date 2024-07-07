38a8705c51e3af5a2d4190698b11b7ec89a85968
julieta <j.gazzin@gmail.com>
2024-07-07 01:55:43 -0300

    crear mensajes ok + carrito en todas las páginas + comparacion x id en carrito

--- public/js/app.js
+++ public/js/app.js
@@ -66,3 +66,76 @@ logout.addEventListener('click', ()=>{
     saludoUser.textContent = ''
     saludoUser.setAttribute("data-id-user", '')
 })
+
+// -------- CARRITO DESDE SESSION STG
+let productosEnSession = JSON.parse(sessionStorage.getItem('carritoSession'))
+console.log(productosEnSession);
+imprimirCarrito()
+
+// FUNCIONES CARRITO
+function imprimirCarrito() {
+    const infoProducto = document.querySelector('.contenedor-carrito .info-producto')
+    console.log(infoProducto);
+
+    // total a pagar
+    const totalPagar = document.querySelector('.contenedor-carrito .precio-total')
+    let total;
+
+    // total
+    total = 0;
+
+    // limpiar html
+    infoProducto.innerHTML = '';
+   
+    // listados productos seleccionados desde sessionSTG
+    if(productosEnSession != null){
+        productosEnSession.forEach( producto => {
+            // console.log(producto);
+            const elementoProducto = document.createElement('div');
+            elementoProducto.classList.add('row-producto');
+            elementoProducto.setAttribute('data-id', producto.id)
+            elementoProducto.innerHTML = `
+                <span class="cantidad">${producto.cant}</span>
+                <span class="nombre">${producto.nombre}</span>
+                <span class="precio">$${producto.precio}</span>
+                <span class="btn-eliminar">x</span>
+            `;
+    
+            infoProducto.append(elementoProducto);
+    
+            // total a pagar
+            total += parseInt(producto.precio) * producto.cant;
+        })
+        totalPagar.innerHTML = `$${total}`
+
+    }
+
+    // eliminar producto
+    infoProducto.addEventListener('click', (e) => {
+        if(e.target.classList.contains('btn-eliminar')) {
+            const producto = e.target.parentElement;
+            const idProd = producto.getAttribute('data-id')
+            //console.log(idProd);
+            productosEnSession = JSON.parse(sessionStorage.getItem('carritoSession'))
+            productosEnSession = productosEnSession.filter(producto => producto.id !== idProd)
+            guardarCarritoSessionlST()
+            imprimirCarrito()
+        }
+    })
+    const vaciar = document.querySelector('#vaciar')
+    vaciar.addEventListener('click', vaciarCarrito)
+
+}
+
+// vaciar carrito
+function vaciarCarrito() {
+    productosEnSession = [];
+    total = 0;
+    guardarCarritoSessionlST()
+    imprimirCarrito()
+}
+
+// guardar en sessionStorage
+function guardarCarritoSessionlST() {
+    sessionStorage.setItem('carritoSession', JSON.stringify(productosEnSession)); 
+}
